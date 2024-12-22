5c048b19f0f877fd306cefcf6d279c07cf36c93f
xiaohuisen <handsomeXiao77@163.com>
2024-12-22 19:59:12 +0800

    11_toRefs与toRef

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,48 +1,35 @@
 <template>
     <div class="person">
-        <h2>一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
-        <button @click="changePrice">修改汽车价格</button>
-        <button @click="changeBrand">修改汽车名</button>
-        <button @click="changeCar">修改汽车</button>
-        <h2>总和：{{ sum }}</h2>
-        <button @click="changeSum">修改总和</button>
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改姓名</button>
+        <button @click="changeAge">修改年龄</button>
     </div>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue';
+import { reactive, toRef, toRefs } from 'vue';
 
 // vue3.2新特性，不用插件，可以定义component的name
 defineOptions({
     name: 'Person'
 })
-// 数据
-// let car = reactive({ brand: '奔驰', price: 100 })
-let car = ref({ brand: '奔驰', price: 100 })
-let sum = ref(0)
+let person = reactive({
+    name: '张三',
+    age: 18
+})
 
-// 方法
-function changePrice() {
-    // car.price += 10 // reactive
-    car.value.price += 10 // ref
+let {name, age} = toRefs(person)
+let nl = toRef(person, 'age')
+
+function changeName(){
+    name.value += '~'
+    console.log(name.value, person.name)
 }
 
-function changeBrand(){
-    // car.brand = '宝马' // reactive
-    car.value.brand = '宝马' // ref
-}
-
-function changeCar(){
-    // car = {brand:'奥拓', price:1} // 不更新
-    // car = reactive({brand:'奥拓', price:1}) // 不更新
-
-    // 更新
-    Object.assign(car, {brand:'奥拓', price:1}) // reactive
-    car.value = {brand:'奥拓', price:1} // ref
-}
-
-function changeSum(){
-    sum.value += 1
+function changeAge(){
+    age.value += 1
+    console.log(age.value, person.age, nl.value)
 }
 
 </script>
