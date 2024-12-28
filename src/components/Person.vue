cf8efe9e45b4a7edf944ab2844becbf059d36387
handsomehs <604707115@qq.com>
2024-12-28 21:21:05 +0800

    16_watch监视，情况4：监视响应式对象中的某个属性

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="person">
-        <h1>情况3：监视【reactive】定义的【对象类型】数据</h1>
+        <h1>情况4：监视响应式对象中的某个属性</h1>
         <h2>姓名：{{ person.name }}</h2>
         <h2>年龄：{{ person.age }}</h2>
+        <h2>汽车：{{ person.car.c1 }}、{{ person.car.c2 }}</h2>
         <button @click="changeName">修改名字</button>
         <button @click="changeAge">修改年龄</button>
-        <button @click="changePerson">修改整个人</button>
+        <button @click="changeCar">修改汽车</button>
+        <button @click="changeC1">修改第一辆车</button>
+        <button @click="changeC2">修改第二辆车</button>
     </div>
 </template>
 
@@ -20,7 +23,11 @@ defineOptions({
 // 数据
 let person = reactive({
     name: '张三',
-    age: 18
+    age: 18,
+    car: {
+        c1: '奔驰',
+        c2: '宝马'
+    }
 })
 
 // 方法
@@ -32,14 +39,27 @@ function changeAge() {
     person.age += 1
 }
 
-function changePerson() {
-    Object.assign(person, { name: '李四', age: 90 })
+function changeCar() {
+    person.car = { c1: '雅迪', c2: '艾玛' }
 }
 
-// 监视，情况3：监视【reactive】定义的【对象类型】数据，默认开启深度监视，该深度监视不能被关闭
-watch(person, (newValue, oldValue) => { // 大部分不关注旧值
-    console.log("person变化了", newValue, oldValue);
-})
+function changeC1() {
+    person.car.c1 = '奥迪'
+}
+
+function changeC2() {
+    person.car.c2 = '大众'
+}
+
+// 监视，情况4：监视响应式对象中的某个属性，且该属性是基本类型的，要写成函数式
+/* watch(()=>person.name, (newValue, oldValue)=>{
+    console.log("person.name变化了", oldValue, newValue);
+}) */
+
+// 监视，情况4：监视响应式对象中的某个属性，且该属性是对象类型的，可以直接写，也能写成函数式，推荐写成函数式，手动开启深度监视
+watch(()=>person.car, (oldValue, newValue)=>{
+    console.log("person.car变化了", oldValue, newValue);
+}, {deep: true})
 
 </script>
 
