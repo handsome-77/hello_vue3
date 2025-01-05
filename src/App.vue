f02b4f764dccd69914db58ce1c07c7fddd92dc55
handsomehs <604707115@qq.com>
2025-01-05 21:34:30 +0800

    27_路由_命名路由

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
         <!-- 导航区 -->
         <div class="navigate">
             <RouterLink to="/home" active-class="active">首页</RouterLink>
-            <RouterLink to="/content" active-class="active">内容</RouterLink>
+            <RouterLink :to="{name: 'neirong'}" active-class="active">内容</RouterLink>
             <RouterLink :to="{path:'/about'}" active-class="active">关于</RouterLink>
         </div>
         <!-- 展示区 -->
--- src/router/index.ts
+++ src/router/index.ts
@@ -11,14 +11,17 @@ const router = createRouter({
     history: createWebHistory(), // 路由器的工作模式
     routes: [ // 路由规则
         {
+            name: 'zhuye',
             path: '/home',
             component: Home
         },
         {
+            name: 'neirong',
             path: '/content',
             component: Content
         },
         {
+            name: 'guanyu',
             path: '/about',
             component: About
         }
