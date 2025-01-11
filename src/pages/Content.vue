7a60c09547b4a15557d302faf2eab81593c21e54
handsomehs <604707115@qq.com>
2025-01-11 17:57:32 +0800

    29_路由_query参数

--- src/pages/Content.vue
+++ src/pages/Content.vue
@@ -3,13 +3,24 @@
         <!-- 导航区 -->
         <ul>
             <li v-for="news in newsList" :key="news.id">
-                <RouterLink :to="{name:'xiangqing'}">{{ news.title }}</RouterLink>
+                <!-- 第一种写法 -->
+                <!-- <RouterLink :to="`/content/detail?id=${news.id}&title=${news.title}&content=${news.content}`">{{ news.title }}</RouterLink> -->
+
+                <!-- 第二种写法 -->
+                <RouterLink :to="{
+                    name: 'xiangqing',
+                    query: {
+                        id: news.id,
+                        title: news.title,
+                        content: news.content
+                    }
+                }">{{ news.title }}</RouterLink>
             </li>
         </ul>
         <!-- 展示区 -->
-         <div class="news-content">
+        <div class="news-content">
             <RouterView></RouterView>
-         </div>
+        </div>
     </div>
 </template>
 
@@ -22,8 +33,8 @@ defineOptions({
 })
 
 const newsList = reactive([
-    {id:'111', title:'新闻1', content:'新闻内容1'},
-    {id:'222', title:'新闻2', content:'新闻内容2'},
-    {id:'333', title:'新闻3', content:'新闻内容3'}
+    { id: '111', title: '新闻1', content: '新闻内容1' },
+    { id: '222', title: '新闻2', content: '新闻内容2' },
+    { id: '333', title: '新闻3', content: '新闻内容3' }
 ])
 </script>
--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -1,15 +1,22 @@
 <template>
     <ul class="news-list">
-        <li>编号：xxx</li>
-        <li>标题：xxx</li>
-        <li>内容：xxx</li>
+        <li>编号：{{ query.id }}</li>
+        <li>标题：{{ query.title }}</li>
+        <li>内容：{{ query.content }}</li>
     </ul>
 </template>
 
 <script lang="ts" setup>
+import { toRefs } from 'vue';
+import { useRoute } from 'vue-router';
+
 defineOptions({
     name: 'Detail'
 })
+
+let route = useRoute()
+let {query} = toRefs(route)
+
 </script>
 
 <style scoped>
