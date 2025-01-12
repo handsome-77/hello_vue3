ab217bc2ed4c2febaff97e88ce37bbdc53290fdb
handsomehs <604707115@qq.com>
2025-01-12 20:21:33 +0800

    36_pinia_搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import { createPinia } from "pinia"
 // 创建一个应用
 const app = createApp(App)
+// 创建pinia
+const pinia = createPinia()
+// 安装pinia
+app.use(pinia)
 // 挂载
 app.mount('#app')
