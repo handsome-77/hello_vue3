a4446334b4396ec93975d5e5701d8842f3bfb5d6
handsomehs <604707115@qq.com>
2025-01-12 21:46:04 +0800

    42_pinia_store组合式写法

--- src/store/loveTalk.ts
+++ src/store/loveTalk.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { nanoid } from "nanoid";
 import { defineStore } from "pinia";
+import { reactive } from "vue";
 
-export const useTalktStore = defineStore('talk', {
+// 选项式
+/* export const useTalktStore = defineStore('talk', {
     actions: {
         async getTalk() {
             // 发请求，连续解构赋值
@@ -19,4 +21,22 @@ export const useTalktStore = defineStore('talk', {
             talkList: JSON.parse(localStorage.getItem('talkList') as string) || []
         }
     }
+}) */
+
+// 组合式
+export const useTalktStore = defineStore('talk', () => {
+    // talkList就是state
+    const talkList = reactive(
+        JSON.parse(localStorage.getItem('talkList') as string) || []
+    )
+    // getTalk函数相当于action
+    async function getTalk() {
+        // 发请求，连续解构赋值
+        let { data: { content } } = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
+        // 把请求回来的字符串，包装成一个对象
+        let obj = { id: nanoid(), content }
+        // 放到数组中
+        talkList.unshift(obj)
+    }
+    return { talkList, getTalk }
 })
